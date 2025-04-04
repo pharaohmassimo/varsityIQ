ecd721b3d703a4bf7c95be299dbb44f08e845aa4
Pharaoh Massimo <pharaoh.masimo@gmail.com>
2025-04-04 22:17:23 +0200

    created common layout for sign and sign up, designed the forms, utilized prop type to differentiate how the form shows on either of the two forms

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,11 +1,17 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
 const page = () => {
-  return (
-    <div>
-      Sign In 
-    </div>
-  )
+  return <AuthForm type = "sign-in"/>
 }
 
 export default page
+
+
+
+
+
+
+
+{/***prop type = "sign in " used to differentiate how the form shows on either of the two pages*/}
+
