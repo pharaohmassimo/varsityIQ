b0b3ec467c367a37e843cea6bba2e8b87c955d22
Pharaoh Massimo <pharaoh.masimo@gmail.com>
2025-04-05 23:19:16 +0200

    updated my firebase client.ts configs for more security

--- firebase/client.ts
+++ firebase/client.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCiCNJPBrrEo6TLjFGT8a8VkRnk9_a14hQ",
-  authDomain: "varsityiq-57118.firebaseapp.com",
-  projectId: "varsityiq-57118",
-  storageBucket: "varsityiq-57118.firebasestorage.app",
-  messagingSenderId: "844720832711",
-  appId: "1:844720832711:web:393a19fbb68393425c3ee2",
-  measurementId: "G-M30JTPKDVH"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
