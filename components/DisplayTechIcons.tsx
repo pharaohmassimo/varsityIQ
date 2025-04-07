138f22d0de33b877769d7f9f4fc73f0f32a2fd2a
Pharaoh Massimo <pharaoh.masimo@gmail.com>
2025-04-07 15:12:40 +0200

    implemented interview generation using gemini ai, tested the endpoints using postman

--- components/DisplayTechIcons.tsx
+++ components/DisplayTechIcons.tsx
@@ -9,7 +9,7 @@ const DisplayTechIcons = async ({techStack} : TechIconProps) => {
       {techIcons.slice(0,3).map(({ tech, url }, index ) => (
         <div key={tech} className='relative group bg-dark-300 rounded-full p-2 flex-center'>
             <span className='tech-tooltip'>{tech}</span>
-            <Image 
+            <Image  
             src={url}
             alt={tech}
             width={100}
